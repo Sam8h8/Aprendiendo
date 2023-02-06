97afa4197f3e49d70e5195d0dd65dc93cbfe0e3c
Samanthina <samanthayamile.pm@gmail.com>
2023-02-06 14:18:24 -0600

    mi primer codigo en c

---
+++ hola mundo/hola.c
@@ -0,0 +1,15 @@
+//Headers - Encabezados
+#include <stdio.h>
+
+// Macros 
+// Global variables - Variables Globales
+// Function prototypes - prototipos de función
+// Main function - Función principal
+
+int main (){
+	printf ("Hola mundo\n");
+    printf ("Hola yo soy Samantha\n");	
+    printf ("Este es mi primer programa\n");
+	return 0;
+}
+// Functions - Funciones 
